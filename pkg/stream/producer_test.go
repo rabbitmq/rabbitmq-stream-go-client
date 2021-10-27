470a90678830a97df63d42754a827c819843825c
Gabriele Santomaggio <g.santomaggio@gmail.com>
2021-10-27 12:55:55 +0200

    fix example

--- pkg/stream/producer_test.go
+++ pkg/stream/producer_test.go
@@ -198,8 +198,7 @@ var _ = Describe("Streaming Producers", func() {
 					Expect(msg.GetPublishingIdAssigned()).To(Equal(int64(i + 1)))
 					Expect(msg.IsConfirmed()).To(Equal(true))
 					Expect(msg.message.GetPublishingId()).To(Equal(int64(-1)))
-					body := fmt.Sprintf("%s", msg.message.GetData()[0])
-					Expect(body).To(Equal("test_" + strconv.Itoa(i)))
+					Expect(msg.message.GetData()[0]).To(Equal("test_" + strconv.Itoa(i)))
 				}
 			}
 		}(chConfirm, producer)
