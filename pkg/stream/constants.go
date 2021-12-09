0cff0383483625d59cc6d89e544b3d47b9bba95b
Gabriele Santomaggio <G.santomaggio@gmail.com>
2021-12-09 12:15:08 +0100

    change the versioning format like the others
    stream clients.
    Promoted to 1.0.0-rc.5

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -19,7 +19,7 @@ const (
 const initBufferPublishSize = 2 + 2 + 1 + 4
 
 const (
-	ClientVersion = "0.1.0-RC4"
+	ClientVersion = "1.0.0-rc.5"
 
 	commandDeclarePublisher       = 1
 	commandPublish                = 2
