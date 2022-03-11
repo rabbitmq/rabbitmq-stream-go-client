628b6ec404f7f8bfdbfcdd7ab42cdd58384b9e88
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-03-11 11:30:13 +0100

    update to rc12

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -19,7 +19,7 @@ const (
 const initBufferPublishSize = 2 + 2 + 1 + 4
 
 const (
-	ClientVersion = "1.0.0-rc11"
+	ClientVersion = "1.0.0-rc12"
 
 	commandDeclarePublisher       = 1
 	commandPublish                = 2
