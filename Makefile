rabbitmq/rabbitmq-stream-go-client

commit d1884c3a5e16ef5a12de70be935df991386357ab

Refactor stream package (#150)
* Refactor socket module

Stop using atomic module, in favour of a pure mutex approach. Atomic has
a few caveats in different platforms e.g. ARMv64.

Minor tweak to the Makefile to allow other container technologies (e.g.
containerd) to start RabbitMQ container for testing.

Signed-off-by: Aitor Perez Cedres <acedres@vmware.com>

* Refactor stream module tests

Moved the before/after suite to the suite setup module, as it is a good
practice in Ginkgo framework.

Changed some sleepy waiting by an assertion to environment being "open".
It is better to assert that the state is what we expect, instead of
delaying the test start with a sleep. After the sleep, we are still
unsure about the state and it may not be correct. It is better to always
assert on expected state, if possible.

Signed-off-by: Aitor Perez Cedres <acedres@vmware.com>

* Update Makefile

Since we are using Ginkgo, it makes sense to use Ginkgo to run our
tests. We get the same options as with go test.

To use Ginkgo, some additional dependencies from Ginkgo are required.
Manually fetched those using go get to current version of Ginkgo.

Signed-off-by: Aitor Perez Cedres <acedres@vmware.com>

* Update go routines in tests

Go routines that make Gomega assertions will panic if the assertion
fails. Ginkgo won't be able to capture the failed assertion without
deferring a GinkgoRecover() call in said go routines.

Signed-off-by: Aitor Perez Cedres <acedres@vmware.com>

Makefile
@@ -22,8 +22,23 @@ $(STATICCHECK):
 check: $(STATICCHECK)
 	$(STATICCHECK) ./pkg/stream
 
+GOMOCK ?= $(GOBIN)/mockgen
+GOMOCK_VERSION ?= v1.6.0
+$(GOMOCK):
+	go install github.com/golang/mock/mockgen@$(GOMOCK_VERSION)
+
+.PHONY: gomock
+gomock: $(GOMOCK)
+
+NUM_PROCS ?= 2
+TEST_TIMEOUT ?= 2m
 test: vet fmt check
-	go test -race -tags debug -v -cpu 2 ./pkg/stream -coverprofile coverage.txt -covermode atomic -ginkgo.v
+	go run github.com/onsi/ginkgo/v2/ginkgo -r --procs=$(NUM_PROCS) --compilers=$(NUM_PROCS) \
+		--randomize-all --randomize-suites \
+		--cover --coverprofile=coverage.txt --covermode=atomic \
+		--race --trace \
+		--tags debug \
+		--timeout=$(TEST_TIMEOUT)
 
 build-all: vet fmt check build-darwin build-windows build-linux
 
@@ -46,18 +61,22 @@ perf-test-help: perf-test-build
 perf-test-build: vet fmt check
 	go build -ldflags=$(LDFLAGS) -o bin/stream-perf-test perfTest/perftest.go
 
+BUILDKIT ?= docker
 perf-test-docker-build: perf-test-build
-	docker build -t pivotalrabbitmq/go-stream-perf-test:$(VERSION) .
+	$(BUILDKIT) build -t pivotalrabbitmq/go-stream-perf-test:$(VERSION) .
 
 perf-test-docker-push: perf-test-docker-build
-	docker push pivotalrabbitmq/go-stream-perf-test:$(VERSION)
+	$(BUILDKIT) push pivotalrabbitmq/go-stream-perf-test:$(VERSION)
 
+RABBITMQ_OCI ?= pivotalrabbitmq/rabbitmq-stream
+BUILDKIT_RUN_ARGS ?= --pull always
+.PHONY: rabbitmq-server
 rabbitmq-server:
-	docker run -it --rm --name rabbitmq-stream-go-client-test \
+	$(BUILDKIT) run -it --rm --name rabbitmq-stream-go-client-test \
 		-p 5552:5552 -p 5672:5672 -p 15672:15672 \
 		-e RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS="-rabbitmq_stream advertised_host localhost" \
-		--pull always \
-		pivotalrabbitmq/rabbitmq-stream
+		$(BUILDKIT_RUN_ARGS) \
+		$(RABBITMQ_OCI)
 
 rabbitmq-ha-proxy:
 	cd compose/ha_tls; rm -rf tls-gen;

go.sum
@@ -9,6 +9,7 @@ github.com/frankban/quicktest v1.14.2 h1:SPb1KFFmM+ybpEjPUhCCkZOM5xlovT5UbrMvWnX
 github.com/frankban/quicktest v1.14.2/go.mod h1:mgiwOwqx65TmIk1wJ6Q7wvnVMocbUorkibMOrVTHZps=
 github.com/fsnotify/fsnotify v1.4.7/go.mod h1:jwhsz4b93w/PPRr/qN1Yymfu8t87LnFCMoQvtojpjFo=
 github.com/fsnotify/fsnotify v1.4.9/go.mod h1:znqG4EE+3YCdAaPaxE2ZRY/06pZUdp0tY4IgpuI1SZQ=
+github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 h1:p104kn46Q8WdvHunIJ9dAyjPVtrBPhSr3KT2yUst43I=
 github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0/go.mod h1:fyg7847qk6SyHyPtNmDHnmrv/HOrqktSC+C9fM+CJOE=
 github.com/golang/protobuf v1.2.0/go.mod h1:6lQm79b+lXiMfvg/cZm0SGofjICqVBUtrP5yJMmIC1U=
 github.com/golang/protobuf v1.4.0-rc.1/go.mod h1:ceaxUfeHdC40wWswd/P6IGgMaK3YpKi5j83Wpe3EHw8=
@@ -28,6 +29,7 @@ github.com/google/go-cmp v0.4.0/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/
 github.com/google/go-cmp v0.5.5/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
 github.com/google/go-cmp v0.5.7 h1:81/ik6ipDQS2aGcBfIN5dHDB36BwrStyeAQquSYCV4o=
 github.com/google/go-cmp v0.5.7/go.mod h1:n+brtR0CgQNWTVd5ZUFpTBC8YFBDLK/h/bpaJ8/DtOE=
+github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 h1:yAJXTCF9TqKcTiHJAE8dj7HMvPfh66eeA2JYW7eFpSE=
 github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38/go.mod h1:kpwsk12EmLew5upagYY7GY0pfYCcupk39gWOCRROcvE=
 github.com/google/uuid v1.3.0 h1:t6JiXgmwXMjEs8VusXIJk2BXHsn+wx8BZdTaoZ5fu7I=
 github.com/google/uuid v1.3.0/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
@@ -123,10 +125,10 @@ golang.org/x/text v0.3.7/go.mod h1:u+2+/6zg+i71rQMx5EYifcz6MCKuco9NR6JIITiCfzQ=
 golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
 golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
 golang.org/x/tools v0.0.0-20201224043029-2b0845dc783e/go.mod h1:emZCQorbCU4vsT4fOWvOPXz4eW1wZW4PmDk9uLelYpA=
+golang.org/x/tools v0.1.10 h1:QjFRCZxdOhBJ/UNgnBZLbNV13DlbnK0quyivTnXJM20=
 golang.org/x/tools v0.1.10/go.mod h1:Uh6Zz+xoGYZom868N8YTex3t7RhtHDBrE8Gzo9bV56E=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
-golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 h1:E7g+9GITq07hpfrRu66IVDexMakfv52eLZ2CXBWiKr4=
 golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 h1:go1bK/D/BFZV2I8cIQd1NKEZ+0owSTG1fDTci4IqFcE=
 golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=

go.mod
@@ -15,11 +15,15 @@ require (
 
 require (
 	github.com/frankban/quicktest v1.14.2 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect

pkg/stream/client_test.go
@@ -2,7 +2,6 @@ package stream
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,37 +9,22 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-const testVhost = "rabbitmq-streams-go-test"
-
-var testEnvironment *Environment
-var testStreamName string
-
-var _ = SynchronizedBeforeSuite(func() []byte {
-	err := createVhost(testVhost)
-	Expect(err).NotTo(HaveOccurred())
-	return nil
-}, func(_ []byte) {
-	//SetLevelInfo(logs.DEBUG)
-	client, err := NewEnvironment(nil)
-	testEnvironment = client
-	Expect(err).NotTo(HaveOccurred())
-	testStreamName = uuid.New().String()
-})
-
-var _ = SynchronizedAfterSuite(func() {
-	time.Sleep(400 * time.Millisecond)
-	Expect(testEnvironment.Close()).NotTo(HaveOccurred())
-	time.Sleep(100 * time.Millisecond)
-}, func() {
-	Expect(deleteVhost(testVhost)).NotTo(HaveOccurred())
-})
-
 var _ = Describe("Streaming testEnvironment", func() {
+	var (
+		testStreamName  string
+		testEnvironment *Environment
+	)
+
 	BeforeEach(func() {
-		time.Sleep(200 * time.Millisecond)
+		client, err := NewEnvironment(nil)
+		Expect(err).NotTo(HaveOccurred())
+		testEnvironment = client
+		testStreamName = uuid.New().String()
 	})
+
 	AfterEach(func() {
-		time.Sleep(200 * time.Millisecond)
+		Expect(testEnvironment.Close()).To(Succeed())
+		Eventually(testEnvironment.IsClosed, time.Millisecond*300).Should(BeTrue(), "Expected testEnvironment to be closed")
 	})
 
 	It("Create Stream", func() {
@@ -150,33 +134,3 @@ var _ = Describe("Streaming testEnvironment", func() {
 			To(ContainSubstring("stream Name can't be empty"))
 	})
 })
-
-func createVhost(vhost string) error {
-	return httpCall("PUT", vhostUrl(vhost))
-}
-
-func deleteVhost(vhost string) error {
-	return httpCall("DELETE", vhostUrl(vhost))
-}
-
-func vhostUrl(vhost string) string {
-	return fmt.Sprintf("http://guest:guest@localhost:15672/api/vhosts/%s", vhost)
-}
-
-func httpCall(method, url string) error {
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-	resp.Body.Close()
-
-	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return fmt.Errorf("http error (%d): %s", resp.StatusCode, resp.Status)
-	}
-	return nil
-}

pkg/stream/consumer_test.go
@@ -272,6 +272,7 @@ var _ = Describe("Streaming Consumers", func() {
 
 		chConfirm := producer.NotifyPublishConfirmation()
 		go func(ch ChannelPublishConfirm, p *Producer) {
+			defer GinkgoRecover()
 			for ids := range ch {
 				for _, msg := range ids {
 					Expect(msg.GetError()).NotTo(HaveOccurred())
@@ -420,6 +421,7 @@ var _ = Describe("Streaming Consumers", func() {
 
 		chConfirm := producer.NotifyPublishConfirmation()
 		go func(ch ChannelPublishConfirm, p *Producer) {
+			defer GinkgoRecover()
 			for ids := range ch {
 				for _, msg := range ids {
 					Expect(msg.GetMessage().GetMessageProperties().To).To(Equal("ToTest"))
@@ -473,6 +475,7 @@ var _ = Describe("Streaming Consumers", func() {
 
 		chConfirm := producer.NotifyPublishConfirmation()
 		go func(ch ChannelPublishConfirm, p *Producer) {
+			defer GinkgoRecover()
 			for ids := range ch {
 				for _, msg := range ids {
 					Expect(msg.GetMessage().GetApplicationProperties()["key1"]).To(Equal("value1"))

pkg/stream/enviroment.go
@@ -18,6 +18,7 @@ type Environment struct {
 	producers *producersEnvironment
 	consumers *consumersEnvironment
 	options   *EnvironmentOptions
+	closed    bool
 }
 
 func NewEnvironment(options *EnvironmentOptions) (*Environment, error) {
@@ -70,6 +71,7 @@ func NewEnvironment(options *EnvironmentOptions) (*Environment, error) {
 		options:   options,
 		producers: newProducers(options.MaxProducersPerClient),
 		consumers: newConsumerEnvironment(options.MaxConsumersPerClient),
+		closed:    false,
 	}, client.connect()
 }
 func (env *Environment) newReconnectClient() (*Client, error) {
@@ -234,7 +236,12 @@ func (env *Environment) NewConsumer(streamName string,
 func (env *Environment) Close() error {
 	_ = env.producers.close()
 	_ = env.consumers.close()
+	env.closed = true
 	return nil
+}
+
+func (env *Environment) IsClosed() bool {
+	return env.closed
 }
 
 type EnvironmentOptions struct {
@@ -435,11 +442,11 @@ func (cc *environmentCoordinator) maybeCleanClients() {
 	}
 }
 
-func (client *Client) maybeCleanProducers(streamName string) {
-	client.mutex.Lock()
-	for pidx, producer := range client.coordinator.Producers() {
+func (c *Client) maybeCleanProducers(streamName string) {
+	c.mutex.Lock()
+	for pidx, producer := range c.coordinator.Producers() {
 		if producer.(*Producer).GetStreamName() == streamName {
-			err := client.coordinator.RemoveProducerById(pidx.(uint8), Event{
+			err := c.coordinator.RemoveProducerById(pidx.(uint8), Event{
 				Command:    CommandMetadataUpdate,
 				StreamName: streamName,
 				Name:       producer.(*Producer).GetName(),
@@ -451,20 +458,20 @@ func (client *Client) maybeCleanProducers(streamName string) {
 			}
 		}
 	}
-	client.mutex.Unlock()
-	if client.coordinator.ProducersCount() == 0 {
-		err := client.Close()
+	c.mutex.Unlock()
+	if c.coordinator.ProducersCount() == 0 {
+		err := c.Close()
 		if err != nil {
 			return
 		}
 	}
 }
 
-func (client *Client) maybeCleanConsumers(streamName string) {
-	client.mutex.Lock()
-	for pidx, consumer := range client.coordinator.consumers {
+func (c *Client) maybeCleanConsumers(streamName string) {
+	c.mutex.Lock()
+	for pidx, consumer := range c.coordinator.consumers {
 		if consumer.(*Consumer).options.streamName == streamName {
-			err := client.coordinator.RemoveConsumerById(pidx.(uint8), Event{
+			err := c.coordinator.RemoveConsumerById(pidx.(uint8), Event{
 				Command:    CommandMetadataUpdate,
 				StreamName: streamName,
 				Name:       consumer.(*Consumer).GetName(),
@@ -476,9 +483,9 @@ func (client *Client) maybeCleanConsumers(streamName string) {
 			}
 		}
 	}
-	client.mutex.Unlock()
-	if client.coordinator.ConsumersCount() == 0 {
-		err := client.Close()
+	c.mutex.Unlock()
+	if c.coordinator.ConsumersCount() == 0 {
+		err := c.Close()
 		if err != nil {
 			return
 		}
@@ -601,7 +608,7 @@ func (cc *environmentCoordinator) newConsumer(leader *Broker, tcpParameters *TCP
 	return subscriber, nil
 }
 
-func (cc *environmentCoordinator) close() error {
+func (cc *environmentCoordinator) Close() error {
 	cc.mutexContext.Lock()
 	defer cc.mutexContext.Unlock()
 	for _, client := range cc.clientsPerContext {
@@ -672,7 +679,7 @@ func (ps *producersEnvironment) close() error {
 	ps.mutex.Lock()
 	defer ps.mutex.Unlock()
 	for _, coordinator := range ps.producersCoordinator {
-		_ = coordinator.close()
+		_ = coordinator.Close()
 	}
 	return nil
 }
@@ -735,7 +742,7 @@ func (ps *consumersEnvironment) close() error {
 	ps.mutex.Lock()
 	defer ps.mutex.Unlock()
 	for _, coordinator := range ps.consumersCoordinator {
-		_ = coordinator.close()
+		_ = coordinator.Close()
 	}
 	return nil
 }

pkg/stream/enviroment_test.go
@@ -11,6 +11,7 @@ import (
 )
 
 var _ = Describe("Environment test", func() {
+	const testVhost = "rabbitmq-streams-go-test"
 
 	It("Multi Producers", func() {
 		env, err := NewEnvironment(nil)
@@ -72,6 +73,7 @@ var _ = Describe("Environment test", func() {
 		for i := 0; i < 5; i++ {
 			wg.Add(1)
 			go func(wg *sync.WaitGroup) {
+				defer GinkgoRecover()
 				producer, err := env.NewProducer(streamName, nil)
 				Expect(err).NotTo(HaveOccurred())
 				time.Sleep(20 * time.Millisecond)
@@ -104,6 +106,7 @@ var _ = Describe("Environment test", func() {
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func() {
+				defer GinkgoRecover()
 				_, errProd := env.NewProducer(streamNameWillBeDelete, nil)
 				Expect(errProd).NotTo(HaveOccurred())
 				_, errProd = env.NewProducer(streamNameWillBeDeleteAfter, nil)

pkg/stream/producer_test.go
@@ -13,21 +13,25 @@ import (
 	"time"
 )
 
-var testProducerStream string
-
 var _ = Describe("Streaming Producers", func() {
+	var (
+		testEnvironment    *Environment
+		testProducerStream string
+	)
 
 	BeforeEach(func() {
-		time.Sleep(200 * time.Millisecond)
+		client, err := NewEnvironment(nil)
+		Expect(err).NotTo(HaveOccurred())
+		testEnvironment = client
 		testProducerStream = uuid.New().String()
 		Expect(testEnvironment.DeclareStream(testProducerStream, nil)).
 			NotTo(HaveOccurred())
-
 	})
-	AfterEach(func() {
-		time.Sleep(200 * time.Millisecond)
-		Expect(testEnvironment.DeleteStream(testProducerStream)).NotTo(HaveOccurred())
 
+	AfterEach(func() {
+		Expect(testEnvironment.DeleteStream(testProducerStream)).NotTo(HaveOccurred())
+		Expect(testEnvironment.Close()).To(Succeed())
+		Eventually(testEnvironment.IsClosed, time.Millisecond*300).Should(BeTrue(), "Expected testEnvironment to be closed")
 	})
 
 	It("NewProducer/Close Publisher", func() {
@@ -48,6 +52,7 @@ var _ = Describe("Streaming Producers", func() {
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func(wg *sync.WaitGroup) {
+				defer GinkgoRecover()
 				defer wg.Done()
 				producer, err := testEnvironment.NewProducer(testProducerStream, nil)
 				Expect(err).NotTo(HaveOccurred())
@@ -76,8 +81,12 @@ var _ = Describe("Streaming Producers", func() {
 		When("No batching, nor sub-entry. nor compression", func() {
 
 			BeforeEach(func() {
-				producer = createProducer(NewProducerOptions().SetBatchSize(1).SetSubEntrySize(1),
-					&messagesReceived)
+				producer = createProducer(
+					NewProducerOptions().SetBatchSize(1).SetSubEntrySize(1),
+					&messagesReceived,
+					testEnvironment,
+					testProducerStream,
+				)
 				wg.Add(ThreadCount)
 			})
 
@@ -100,8 +109,12 @@ var _ = Describe("Streaming Producers", func() {
 		When("Batching enabled, but without sub-entry and compression", func() {
 
 			BeforeEach(func() {
-				producer = createProducer(NewProducerOptions().SetBatchSize(BatchSize).SetSubEntrySize(1),
-					&messagesReceived)
+				producer = createProducer(
+					NewProducerOptions().SetBatchSize(BatchSize).SetSubEntrySize(1),
+					&messagesReceived,
+					testEnvironment,
+					testProducerStream,
+				)
 				wg.Add(ThreadCount)
 			})
 
@@ -126,7 +139,10 @@ var _ = Describe("Streaming Producers", func() {
 			BeforeEach(func() {
 				producer = createProducer(
 					NewProducerOptions().SetBatchSize(BatchSize).SetSubEntrySize(SubEntrySize).SetCompression(Compression{}.None()),
-					&messagesReceived)
+					&messagesReceived,
+					testEnvironment,
+					testProducerStream,
+				)
 				wg.Add(ThreadCount)
 			})
 
@@ -151,7 +167,10 @@ var _ = Describe("Streaming Producers", func() {
 			BeforeEach(func() {
 				producer = createProducer(
 					NewProducerOptions().SetBatchSize(BatchSize).SetSubEntrySize(SubEntrySize).SetCompression(Compression{}.Gzip()),
-					&messagesReceived)
+					&messagesReceived,
+					testEnvironment,
+					testProducerStream,
+				)
 
 				wg.Add(ThreadCount)
 			})
@@ -186,6 +205,7 @@ var _ = Describe("Streaming Producers", func() {
 		Expect(err).NotTo(HaveOccurred())
 		chConfirm := producer.NotifyPublishConfirmation()
 		go func(ch ChannelPublishConfirm, p *Producer) {
+			defer GinkgoRecover()
 			for ids := range ch {
 				atomic.AddInt32(&messagesReceived, int32(len(ids)))
 				for i, msg := range ids {
@@ -257,6 +277,7 @@ var _ = Describe("Streaming Producers", func() {
 
 		chPublishConfirmation := producer.NotifyPublishConfirmation()
 		go func(ch ChannelPublishConfirm) {
+			defer GinkgoRecover()
 			for msgs := range ch {
 				for _, msg := range msgs {
 					if !msg.IsConfirmed() {
@@ -510,6 +531,7 @@ var _ = Describe("Streaming Producers", func() {
 		Expect(err).NotTo(HaveOccurred())
 		chPublishError := producer.NotifyPublishConfirmation()
 		go func(ch ChannelPublishConfirm) {
+			defer GinkgoRecover()
 			for msgs := range ch {
 				for _, msg := range msgs {
 					if !msg.IsConfirmed() {
@@ -750,12 +772,12 @@ func testCompress(producer *Producer) {
 		"confirm should receive same messages send by producer")
 }
 
-func createProducer(producerOptions *ProducerOptions, messagesReceived *int32) *Producer {
+func createProducer(producerOptions *ProducerOptions, messagesReceived *int32, testEnvironment *Environment, streamName string) *Producer {
 	var err error
 
 	atomic.StoreInt32(messagesReceived, 0)
 
-	producer, err := testEnvironment.NewProducer(testProducerStream, producerOptions)
+	producer, err := testEnvironment.NewProducer(streamName, producerOptions)
 	Expect(err).NotTo(HaveOccurred())
 
 	chConfirm := producer.NotifyPublishConfirmation()
@@ -770,27 +792,22 @@ func createProducer(producerOptions *ProducerOptions, messagesReceived *int32) *
 
 func sendConcurrentlyAndAsynchronously(producer *Producer, threadCount int, wg *sync.WaitGroup, totalMessageCountPerThread int) {
 	runConcurrentlyAndWaitTillAllDone(threadCount, wg, func(goRoutingIndex int) {
-		//fmt.Printf("[%d] Sending %d messages asynchronoulsy\n", goRoutingIndex, totalMessageCountPerThread)
+		defer GinkgoRecover()
 		messagePrefix := fmt.Sprintf("test_%d_", goRoutingIndex)
 		for i := 0; i < totalMessageCountPerThread; i++ {
 			Expect(producer.Send(CreateMessageForTesting(messagePrefix, i))).NotTo(HaveOccurred())
 		}
-		//fmt.Printf("[%d] Sent %d messages\n", goRoutingIndex, totalMessageCountPerThread)
-
 	})
 }
 
 func sendConcurrentlyAndSynchronously(producer *Producer, threadCount int, wg *sync.WaitGroup, totalMessageCountPerThread int, batchSize int) {
 	runConcurrentlyAndWaitTillAllDone(threadCount, wg, func(goRoutingIndex int) {
+		defer GinkgoRecover()
 		totalBatchCount := totalMessageCountPerThread / batchSize
-		//fmt.Printf("[%d] Sending %d messages in batches of %d (total batch:%d) synchronously\n", goRoutingIndex,
-		//			totalMessageCountPerThread, batchSize, totalBatchCount)
 		for batchIndex := 0; batchIndex < totalBatchCount; batchIndex++ {
 			messagePrefix := fmt.Sprintf("test_%d_%d_", goRoutingIndex, batchIndex)
 			Expect(producer.BatchSend(CreateArrayMessagesForTestingWithPrefix(messagePrefix, batchSize))).NotTo(HaveOccurred())
 		}
-		//fmt.Printf("[%d] Sent %d messages\n", goRoutingIndex, totalMessageCountPerThread)
-
 	})
 }
 

commit fc4e23eb93bacc03c29bffde33ebdd452f17c6be

Add missing dependencies for Ginkgo Runner
This was a small regression caused by #150

Signed-off-by: Aitor Perez Cedres <acedres@vmware.com>

Makefile
@@ -33,7 +33,7 @@ gomock: $(GOMOCK)
 NUM_PROCS ?= 2
 TEST_TIMEOUT ?= 2m
 test: vet fmt check
-	go run github.com/onsi/ginkgo/v2/ginkgo -r --procs=$(NUM_PROCS) --compilers=$(NUM_PROCS) \
+	go run -mod=mod github.com/onsi/ginkgo/v2/ginkgo -r --procs=$(NUM_PROCS) --compilers=$(NUM_PROCS) \
 		--randomize-all --randomize-suites \
 		--cover --coverprofile=coverage.txt --covermode=atomic \
 		--race --trace \

go.sum
@@ -9,6 +9,7 @@ github.com/frankban/quicktest v1.14.2 h1:SPb1KFFmM+ybpEjPUhCCkZOM5xlovT5UbrMvWnX
 github.com/frankban/quicktest v1.14.2/go.mod h1:mgiwOwqx65TmIk1wJ6Q7wvnVMocbUorkibMOrVTHZps=
 github.com/fsnotify/fsnotify v1.4.7/go.mod h1:jwhsz4b93w/PPRr/qN1Yymfu8t87LnFCMoQvtojpjFo=
 github.com/fsnotify/fsnotify v1.4.9/go.mod h1:znqG4EE+3YCdAaPaxE2ZRY/06pZUdp0tY4IgpuI1SZQ=
+github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 h1:p104kn46Q8WdvHunIJ9dAyjPVtrBPhSr3KT2yUst43I=
 github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0/go.mod h1:fyg7847qk6SyHyPtNmDHnmrv/HOrqktSC+C9fM+CJOE=
 github.com/golang/protobuf v1.2.0/go.mod h1:6lQm79b+lXiMfvg/cZm0SGofjICqVBUtrP5yJMmIC1U=
 github.com/golang/protobuf v1.4.0-rc.1/go.mod h1:ceaxUfeHdC40wWswd/P6IGgMaK3YpKi5j83Wpe3EHw8=
@@ -28,6 +29,7 @@ github.com/google/go-cmp v0.4.0/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/
 github.com/google/go-cmp v0.5.5/go.mod h1:v8dTdLbMG2kIc/vJvl+f65V22dbkXbowE6jgT/gNBxE=
 github.com/google/go-cmp v0.5.7 h1:81/ik6ipDQS2aGcBfIN5dHDB36BwrStyeAQquSYCV4o=
 github.com/google/go-cmp v0.5.7/go.mod h1:n+brtR0CgQNWTVd5ZUFpTBC8YFBDLK/h/bpaJ8/DtOE=
+github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 h1:yAJXTCF9TqKcTiHJAE8dj7HMvPfh66eeA2JYW7eFpSE=
 github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38/go.mod h1:kpwsk12EmLew5upagYY7GY0pfYCcupk39gWOCRROcvE=
 github.com/google/uuid v1.3.0 h1:t6JiXgmwXMjEs8VusXIJk2BXHsn+wx8BZdTaoZ5fu7I=
 github.com/google/uuid v1.3.0/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
@@ -123,6 +125,7 @@ golang.org/x/text v0.3.7/go.mod h1:u+2+/6zg+i71rQMx5EYifcz6MCKuco9NR6JIITiCfzQ=
 golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
 golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
 golang.org/x/tools v0.0.0-20201224043029-2b0845dc783e/go.mod h1:emZCQorbCU4vsT4fOWvOPXz4eW1wZW4PmDk9uLelYpA=
+golang.org/x/tools v0.1.10 h1:QjFRCZxdOhBJ/UNgnBZLbNV13DlbnK0quyivTnXJM20=
 golang.org/x/tools v0.1.10/go.mod h1:Uh6Zz+xoGYZom868N8YTex3t7RhtHDBrE8Gzo9bV56E=
 golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 golang.org/x/xerrors v0.0.0-20191011141410-1b5146add898/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=