d331aec91aad776da0f5bcc92be716137bc625c7
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-08-07 15:32:57 +0200

    update to 1.0.1-rc.2
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -19,7 +19,7 @@ const (
 const initBufferPublishSize = 2 + 2 + 1 + 4
 
 const (
-	ClientVersion = "1.0.1-rc.1"
+	ClientVersion = "1.0.1-rc.2"
 
 	commandDeclarePublisher       = 1
 	commandPublish                = 2
