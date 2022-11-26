66d10badcde3e93e1057a73124a312db0bd550a4
Gabriele Santomaggio <G.santomaggio@gmail.com>
2022-11-26 09:53:22 +0100

    Publish Work in progress
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- pkg/common/configurations.go
+++ pkg/common/configurations.go
@@ -16,7 +16,7 @@ type Clienter interface {
 }
 
 // TODO: godocs
-type StreamConfiguration map[string]string
+type StreamConfiguration = map[string]string
 
 func NewConfiguration(name string) map[string]string {
 	c := make(StreamConfiguration)
