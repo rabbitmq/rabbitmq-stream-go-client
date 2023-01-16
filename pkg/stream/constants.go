5448d01fb3e2fafce4870f8745d8f7f2c4342781
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-01-16 10:51:34 +0100

    update to 1.0.2 [skip ci]
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -19,7 +19,7 @@ const (
 const initBufferPublishSize = 2 + 2 + 1 + 4
 
 const (
-	ClientVersion = "1.0.1"
+	ClientVersion = "1.0.2"
 
 	commandDeclarePublisher       = 1
 	commandPublish                = 2
