4a5a2f658f480f0367bafa9aa4d19025145392b8
Alex Blease <ablease@vmware.com>
2023-09-28 09:26:44 +0100

    Remove duplication in log handler setup

--- pkg/stream/stream_suite_test.go
+++ pkg/stream/stream_suite_test.go
@@ -1,23 +1,13 @@
 package stream_test
 
 import (
-	"golang.org/x/exp/slog"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-var logger *slog.Logger
-
 func TestStream(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Stream Suite")
 }
-
-var _ = BeforeSuite(func() {
-	h := slog.HandlerOptions{
-		Level: slog.LevelDebug,
-	}.NewTextHandler(GinkgoWriter)
-	logger = slog.New(h)
-})
