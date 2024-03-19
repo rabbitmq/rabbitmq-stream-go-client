04f8336a20df6c1908b43cff8db70096ad17defe
Gabriele Santomaggio <G.santomaggio@gmail.com>
2024-03-19 16:26:32 +0100

    Exchange version (#278)
    * Exchange version command
    
    ---------
    
    Signed-off-by: Gabriele Santomaggio <g.santomaggio@gmail.com>

---
+++ pkg/stream/available_features.go
@@ -0,0 +1,113 @@
+package stream
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var lock = &sync.Mutex{}
+var (
+	instance availableFeatures
+)
+
+type availableFeatures struct {
+	is313OrMore   bool
+	is311OrMore   bool
+	brokerVersion string
+}
+
+func availableFeaturesInstance() *availableFeatures {
+	if instance == (availableFeatures{}) {
+		lock.Lock()
+		defer lock.Unlock()
+		if instance == (availableFeatures{}) {
+			instance = availableFeatures{}
+		}
+	}
+	return &instance
+}
+
+func (a *availableFeatures) Is311OrMore() bool {
+	return a.is311OrMore
+}
+
+func (a *availableFeatures) Is313OrMore() bool {
+	return a.is313OrMore
+}
+
+func (a *availableFeatures) SetVersion(version string) error {
+	if extractVersion(version) == "" {
+		return fmt.Errorf("invalid version format: %s", version)
+	}
+	a.brokerVersion = version
+	a.is311OrMore = IsVersionGreaterOrEqual(extractVersion(version), "3.11.0")
+	a.is313OrMore = IsVersionGreaterOrEqual(extractVersion(version), "3.13.0")
+	return nil
+}
+
+func extractVersion(fullVersion string) string {
+	pattern := `(\d+\.\d+\.\d+)`
+	regex := regexp.MustCompile(pattern)
+	match := regex.FindStringSubmatch(fullVersion)
+
+	if len(match) > 1 {
+		return match[1]
+	}
+	return ""
+}
+
+func IsVersionGreaterOrEqual(version, target string) bool {
+	v1, err := parseVersion(version)
+	if err != nil {
+		return false
+	}
+
+	v2, err := parseVersion(target)
+	if err != nil {
+		return false
+	}
+	return v1.Compare(v2) >= 0
+}
+
+func parseVersion(version string) (Version, error) {
+	parts := strings.Split(version, ".")
+	if len(parts) != 3 {
+		return Version{}, fmt.Errorf("invalid version format: %s", version)
+	}
+
+	major, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return Version{}, fmt.Errorf("invalid major version: %s", parts[0])
+	}
+
+	minor, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return Version{}, fmt.Errorf("invalid minor version: %s", parts[1])
+	}
+
+	patch, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return Version{}, fmt.Errorf("invalid patch version: %s", parts[2])
+	}
+
+	return Version{Major: major, Minor: minor, Patch: patch}, nil
+}
+
+type Version struct {
+	Major int
+	Minor int
+	Patch int
+}
+
+func (v Version) Compare(other Version) int {
+	if v.Major != other.Major {
+		return v.Major - other.Major
+	}
+	if v.Minor != other.Minor {
+		return v.Minor - other.Minor
+	}
+	return v.Patch - other.Patch
+}
---
+++ pkg/stream/available_features_test.go
@@ -0,0 +1,72 @@
+package stream
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Available Features", func() {
+
+	It("Parse Version", func() {
+		v, err := parseVersion("1.2.3")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(v).To(Equal(Version{Major: 1, Minor: 2, Patch: 3}))
+
+		_, err = parseVersion("1.2")
+		Expect(err).To(HaveOccurred())
+		Expect(fmt.Sprintf("%s", err)).To(ContainSubstring("invalid version format: 1.2"))
+
+		_, err = parseVersion("error.3.3")
+		Expect(err).To(HaveOccurred())
+		Expect(fmt.Sprintf("%s", err)).To(ContainSubstring("invalid major version: error"))
+
+		_, err = parseVersion("1.error.3")
+		Expect(err).To(HaveOccurred())
+		Expect(fmt.Sprintf("%s", err)).To(ContainSubstring("invalid minor version: error"))
+
+		_, err = parseVersion("1.2.error")
+		Expect(err).To(HaveOccurred())
+		Expect(fmt.Sprintf("%s", err)).To(ContainSubstring("invalid patch version: error"))
+
+		v, err = parseVersion(extractVersion("3.12.1-rc1"))
+		Expect(err).NotTo(HaveOccurred())
+		Expect(v).To(Equal(Version{Major: 3, Minor: 12, Patch: 1}))
+
+		v, err = parseVersion(extractVersion("3.13.1-alpha.234"))
+		Expect(err).NotTo(HaveOccurred())
+		Expect(v).To(Equal(Version{Major: 3, Minor: 13, Patch: 1}))
+	})
+
+	It("Is Version Greater Or Equal", func() {
+		Expect(IsVersionGreaterOrEqual("1.2.3", "1.2.3")).To(BeTrue())
+		Expect(IsVersionGreaterOrEqual("1.2.3", "1.2.2")).To(BeTrue())
+		Expect(IsVersionGreaterOrEqual("1.2.3", "1.2.4")).To(BeFalse())
+		Expect(IsVersionGreaterOrEqual("1.2.3", "1.3.3")).To(BeFalse())
+		Expect(IsVersionGreaterOrEqual("1.2.3", "2.2.3")).To(BeFalse())
+		Expect(IsVersionGreaterOrEqual("3.1.3-alpha.1", "2.2.3")).To(BeFalse())
+		Expect(IsVersionGreaterOrEqual("3.3.3-rc.1", "2.2.3")).To(BeFalse())
+
+		Expect(IsVersionGreaterOrEqual("error.3.2", "2.2.3")).To(BeFalse())
+		Expect(IsVersionGreaterOrEqual("4.3.2", "2.error.3")).To(BeFalse())
+
+	})
+
+	It("Available Features", func() {
+		var availableFeatures = availableFeaturesInstance()
+		Expect(availableFeatures).NotTo(BeNil())
+		Expect(availableFeatures.SetVersion("error")).NotTo(BeNil())
+		Expect(availableFeatures.SetVersion("3.9.0")).To(BeNil())
+		Expect(availableFeatures.Is311OrMore()).To(BeFalse())
+		Expect(availableFeatures.Is313OrMore()).To(BeFalse())
+		Expect(availableFeatures.SetVersion("3.11.0")).To(BeNil())
+		Expect(availableFeatures.Is311OrMore()).To(BeTrue())
+		Expect(availableFeatures.Is313OrMore()).To(BeFalse())
+		Expect(availableFeatures.SetVersion("3.13.0")).To(BeNil())
+		Expect(availableFeatures.Is311OrMore()).To(BeTrue())
+		Expect(availableFeatures.Is313OrMore()).To(BeTrue())
+		Expect(availableFeatures.SetVersion("3.13.1-alpha.234")).To(BeNil())
+		Expect(availableFeatures.Is311OrMore()).To(BeTrue())
+		Expect(availableFeatures.Is313OrMore()).To(BeTrue())
+	})
+})
