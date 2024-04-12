e82d1e13c7b2286d6ca79c17d861025808d3cf5b
Gabriele Santomaggio <g.santomaggio@gmail.com>
2024-04-12 08:23:09 +0200

    bump version to 1.4
    Signed-off-by: Gabriele Santomaggio <g.santomaggio@gmail.com>

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -19,7 +19,7 @@ const (
 const initBufferPublishSize = 2 + 2 + 1 + 4
 
 const (
-	ClientVersion = "1.3.3"
+	ClientVersion = "1.4.0"
 
 	commandDeclarePublisher       = 1
 	commandPublish                = 2
