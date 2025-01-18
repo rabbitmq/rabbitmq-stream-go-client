298d6fe665949619ee490f1c4d9fce6d04d25866
Alberto Moretti <moretti919@gmail.com>
2025-01-18 18:44:54 +0100

    perf: restore simple map for unconfirmed since it is 15% faster

--- pkg/stream/producer_unconfirmed.go
+++ pkg/stream/producer_unconfirmed.go
@@ -1,52 +1,20 @@
 package stream
 
 import (
-	"container/heap"
 	"sync"
 	"time"
 )
 
-type priorityMessage struct {
-	*ConfirmationStatus
-	index int
-}
-
-// priorityQueue implements heap.Interface
-type priorityQueue []*priorityMessage
-
-func (pq priorityQueue) Len() int { return len(pq) }
-
-func (pq priorityQueue) Less(i, j int) bool {
-	// Earlier timestamps have higher priority
-	return pq[i].inserted.Before(pq[j].inserted)
-}
-
-func (pq priorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *priorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*priorityMessage)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *priorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil
-	item.index = -1
-	*pq = old[0 : n-1]
-	return item
-}
-
+// unConfirmed is a structure that holds unconfirmed messages
+// And unconfirmed message is a message that has been sent to the broker but not yet confirmed,
+// and it is added to the unConfirmed structure as soon is possible when
+//
+//	the Send() or BatchSend() method is called
+//
+// The confirmation status is updated when the confirmation is received from the broker (see server_frame.go)
+// or due of timeout. The Timeout is configurable, and it is calculated client side.
 type unConfirmed struct {
-	messages        map[int64]*priorityMessage
-	timeoutQueue    priorityQueue
+	messages        map[int64]*ConfirmationStatus
 	mutexMessageMap sync.RWMutex
 }
 
@@ -54,11 +22,9 @@ const DefaultUnconfirmedSize = 10_000
 
 func newUnConfirmed() *unConfirmed {
 	r := &unConfirmed{
-		messages:        make(map[int64]*priorityMessage, DefaultUnconfirmedSize),
-		timeoutQueue:    make(priorityQueue, 0, DefaultUnconfirmedSize),
+		messages:        make(map[int64]*ConfirmationStatus, DefaultUnconfirmedSize),
 		mutexMessageMap: sync.RWMutex{},
 	}
-	heap.Init(&r.timeoutQueue)
 	return r
 }
 
@@ -67,30 +33,23 @@ func (u *unConfirmed) addFromSequences(messages []*messageSequence, producerID u
 	defer u.mutexMessageMap.Unlock()
 
 	for _, msgSeq := range messages {
-		pm := &priorityMessage{
-			ConfirmationStatus: &ConfirmationStatus{
-				inserted:     time.Now(),
-				message:      msgSeq.sourceMsg,
-				producerID:   producerID,
-				publishingId: msgSeq.publishingId,
-				confirmed:    false,
-			},
+		u.messages[msgSeq.publishingId] = &ConfirmationStatus{
+			inserted:     time.Now(),
+			message:      msgSeq.sourceMsg,
+			producerID:   producerID,
+			publishingId: msgSeq.publishingId,
+			confirmed:    false,
 		}
-		u.messages[msgSeq.publishingId] = pm
-		heap.Push(&u.timeoutQueue, pm)
+
 	}
 }
 
 func (u *unConfirmed) link(from int64, to int64) {
 	u.mutexMessageMap.Lock()
 	defer u.mutexMessageMap.Unlock()
-
-	fromMsg := u.messages[from]
-	if fromMsg != nil {
-		toMsg := u.messages[to]
-		if toMsg != nil {
-			fromMsg.linkedTo = append(fromMsg.linkedTo, toMsg.ConfirmationStatus)
-		}
+	r := u.messages[from]
+	if r != nil {
+		r.linkedTo = append(r.linkedTo, u.messages[to])
 	}
 }
 
@@ -100,14 +59,16 @@ func (u *unConfirmed) extractWithConfirms(ids []int64) []*ConfirmationStatus {
 
 	res := make([]*ConfirmationStatus, 0, len(ids))
 	for _, v := range ids {
-		if msg := u.extract(v, 0, true); msg != nil {
-			res = append(res, msg)
-			if msg.linkedTo != nil {
-				res = append(res, msg.linkedTo...)
+		m := u.extract(v, 0, true)
+		if m != nil {
+			res = append(res, m)
+			if m.linkedTo != nil {
+				res = append(res, m.linkedTo...)
 			}
 		}
 	}
 	return res
+
 }
 
 func (u *unConfirmed) extractWithError(id int64, errorCode uint16) *ConfirmationStatus {
@@ -117,53 +78,38 @@ func (u *unConfirmed) extractWithError(id int64, errorCode uint16) *Confirmation
 }
 
 func (u *unConfirmed) extract(id int64, errorCode uint16, confirmed bool) *ConfirmationStatus {
-	pm := u.messages[id]
-	if pm == nil {
-		return nil
-	}
+	rootMessage := u.messages[id]
+	if rootMessage != nil {
+		u.updateStatus(rootMessage, errorCode, confirmed)
 
-	rootMessage := pm.ConfirmationStatus
-	rootMessage.updateStatus(errorCode, confirmed)
-
-	for _, linkedMessage := range rootMessage.linkedTo {
-		linkedMessage.updateStatus(errorCode, confirmed)
-		if linkedPm := u.messages[linkedMessage.publishingId]; linkedPm != nil {
-			// Remove from priority queue if exists
-			if linkedPm.index != -1 {
-				heap.Remove(&u.timeoutQueue, linkedPm.index)
-			}
+		for _, linkedMessage := range rootMessage.linkedTo {
+			u.updateStatus(linkedMessage, errorCode, confirmed)
 			delete(u.messages, linkedMessage.publishingId)
 		}
+		delete(u.messages, id)
 	}
-
-	// Remove from priority queue if exists
-	if pm.index != -1 {
-		heap.Remove(&u.timeoutQueue, pm.index)
-	}
-	delete(u.messages, id)
-
 	return rootMessage
+}
+
+func (u *unConfirmed) updateStatus(rootMessage *ConfirmationStatus, errorCode uint16, confirmed bool) {
+	rootMessage.confirmed = confirmed
+	if confirmed {
+		return
+	}
+	rootMessage.errorCode = errorCode
+	rootMessage.err = lookErrorCode(errorCode)
 }
 
 func (u *unConfirmed) extractWithTimeOut(timeout time.Duration) []*ConfirmationStatus {
 	u.mutexMessageMap.Lock()
 	defer u.mutexMessageMap.Unlock()
-
 	var res []*ConfirmationStatus
-	now := time.Now()
-
-	for u.timeoutQueue.Len() > 0 {
-		pm := u.timeoutQueue[0]
-		if now.Sub(pm.inserted) < timeout {
-			break
-		}
-
-		heap.Pop(&u.timeoutQueue)
-		if msg := u.extract(pm.publishingId, timeoutError, false); msg != nil {
-			res = append(res, msg)
+	for _, v := range u.messages {
+		if time.Since(v.inserted) > timeout {
+			v := u.extract(v.publishingId, timeoutError, false)
+			res = append(res, v)
 		}
 	}
-
 	return res
 }
 
