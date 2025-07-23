ce445c9434fdb5662ea8facfcebd74b830a32124
Joeson Huang <joesonw@gmail.com>
2025-07-23 16:36:59 -0400

    allow user replaces logger

--- pkg/logs/log.go
+++ pkg/logs/log.go
@@ -10,22 +10,49 @@ const (
 	DEBUG = 1
 )
 
+type Logger interface {
+	Info(message string, v ...any)
+	Error(message string, v ...any)
+	Debug(message string, v ...any)
+	Warn(message string, v ...any)
+}
+
+var DefaultLogger Logger = defaultLogger{}
+
 var LogLevel int8
 
 func LogInfo(message string, v ...any) {
-	log.Printf(fmt.Sprintf("[info] - %s", message), v...)
+	DefaultLogger.Info(message, v...)
 }
 
 func LogError(message string, v ...any) {
-	log.Printf(fmt.Sprintf("[error] - %s", message), v...)
+	DefaultLogger.Error(message, v...)
 }
 
 func LogDebug(message string, v ...any) {
+	DefaultLogger.Debug(message, v...)
+}
+
+func LogWarn(message string, v ...any) {
+	DefaultLogger.Warn(message, v...)
+}
+
+type defaultLogger struct{}
+
+func (l defaultLogger) Info(message string, v ...any) {
+	log.Printf(fmt.Sprintf("[info] - %s", message), v...)
+}
+
+func (l defaultLogger) Error(message string, v ...any) {
+	log.Printf(fmt.Sprintf("[error] - %s", message), v...)
+}
+
+func (l defaultLogger) Debug(message string, v ...any) {
 	if LogLevel > INFO {
 		log.Printf(fmt.Sprintf("[debug] - %s", message), v...)
 	}
 }
 
-func LogWarn(message string, v ...any) {
+func (l defaultLogger) Warn(message string, v ...any) {
 	log.Printf(fmt.Sprintf("[warn] - %s", message), v...)
 }
