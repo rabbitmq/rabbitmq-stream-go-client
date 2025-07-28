c192492b9ec84e00c7ff79b8b22f1f632bc1775c
Gabriele Santomaggio <G.santomaggio@gmail.com>
2025-07-28 15:16:07 +0200

    fix lint
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- pkg/stream/client.go
+++ pkg/stream/client.go
@@ -3,6 +3,7 @@ package stream
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"crypto/tls"
 	"errors"
 	"fmt"
@@ -693,7 +694,8 @@ func (c *Client) BrokerLeader(stream string) (*Broker, error) {
 	streamMetadata.Leader.advHost = streamMetadata.Leader.Host
 
 	// see: https://github.com/rabbitmq/rabbitmq-stream-go-client/pull/317
-	_, err := net.LookupIP(streamMetadata.Leader.Host)
+	res := &net.Resolver{}
+	_, err := res.LookupIPAddr(context.Background(), streamMetadata.Leader.Host)
 	if err != nil {
 		var dnsError *net.DNSError
 		if errors.As(err, &dnsError) {
