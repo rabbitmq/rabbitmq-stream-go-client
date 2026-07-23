e56918bc2858425622a7bd8f569d46daecba289a
Gabriele Santomaggio <G.santomaggio@gmail.com>
2026-07-23 10:23:18 +0200

    rabbitmq-stream-go-client v1.8.3

--- pkg/stream/constants.go
+++ pkg/stream/constants.go
@@ -19,7 +19,7 @@ const (
 const initBufferPublishSize = 2 + 2 + 1 + 4
 
 const (
-	ClientVersion = "1.8.2"
+	ClientVersion = "1.8.3"
 
 	commandDeclarePublisher       = 1
 	commandPublish                = 2
